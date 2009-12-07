6492fd3d69269db05fddaffe1ccf23c825535cb9
Grzegorz Anielak <g.anielak@gmail.com>
2009-12-07 23:06:25 +0100

    Move gen_wrapper functionality to match.py.

--- hammer/match.py
+++ hammer/match.py
@@ -3,37 +3,57 @@
 
 # Runs one match and return program-oriented (second parameter) score
 
-import random, sys
+import random, sys, os, stat
 from subprocess import Popen, PIPE
-from tempfile import mkstemp
+from tempfile import NamedTemporaryFile
 
-if len(sys.argv) != 3:
-    print >> sys.stderr, 'USAGE: match.py <base_program> <program>'
+def MakeWrapper(program, parameters):
+    wrapper = NamedTemporaryFile(prefix='wrapper-', dir='.', delete=False)
+
+    print >>wrapper, r'''#!/bin/bash
+function read_eqs()
+{
+    for i in `seq $1`; do
+        read line
+        read line
+    done
+    cat
+}'''
+    print >>wrapper,'cat', parameters,'- |',program, '| read_eqs `wc -l <', parameters, '`'
+
+    wrapper.close()
+    os.chmod(wrapper.name, stat.S_IRWXU)
+    return wrapper.name
+
+def Match(base_program, program):
+    first = random.randint(0, 1) #which program is about to play first
+    programs = [base_program, program]
+    judge = Popen(
+        ['./judge-static', '-s', '-P 0', '-i', 'plik_11.txt',
+            programs[first], programs[1 - first]],
+        stdout=PIPE)
+    result = judge.wait()
+
+    if result != 0:
+        print >> sys.stderr, 'match.py: judge-static returned error'
+        exit(-1)
+
+    line = judge.stdout.readlines()[-1].split('=')
+    score = int(line[1].split(' ')[0])
+    #time_used = int(line[3].split(' ')[0])
+    if first == 0:
+        return score
+    else:
+        return 1 - score
+
+if len(sys.argv) != 4:
+    print >> sys.stderr, 'USAGE: match.py <base_program> <program> <parameters file>'
     exit(-1)
 
 base_program = sys.argv[1]
 program = sys.argv[2]
-first = random.randint(0, 1) #which program is about to play first
+parameters = sys.argv[3]
 
-programs = [base_program, program]
-judge = Popen(
-    ['./judge-static', '-s', '-P 0', '-i', 'plik_11.txt',
-        programs[first], programs[1 - first]],
-    stdout=PIPE)
-result = judge.wait()
-
-if result != 0:
-    print >> sys.stderr, 'match.py: judge-static returned error'
-    exit(-1)
-
-def Extract(line):
-    line = line.split('=')
-    score = int(line[1].split(' ')[0])
-    #time_used = int(line[3].split(' ')[0])
-    return score
-
-score = Extract(judge.stdout.readlines()[-1])
-if first == 0:
-    print score
-else:
-    print 1 - score
+wrapper = MakeWrapper(program, parameters)
+print Match(base_program, wrapper)
+os.remove(wrapper)
